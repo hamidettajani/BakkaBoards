0d99ce66b46cccb36ef4a72ddf9fe8a52b7b519a
KungBao <96529195+KungBao82045@users.noreply.github.com>
2022-11-21 11:26:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import random
+import pandas
 
 user = input("Skriv inn brukenavnet ditt: ")
 exampleScore = random.randint(0,100)
 
-with open("leaderboard.txt", "a") as leaderboard:
-    leaderboard.write(f"{user} | Score: {exampleScore}\n")
+with open("leaderboard.csv", "a") as leaderboard:
+    leaderboard.write(f"{user}, {exampleScore}\n")
     leaderboard.close()
+
+
+with open("leaderboard.csv", "r") as file:
+    data = pandas.read_csv(file)
+    print("\n")
+    print(data.sort_values(by="Score"))
