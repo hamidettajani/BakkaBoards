79f2c7e43c7ab9ec5617b22c4638ccc4689b68a9
KungBao <96529195+KungBao82045@users.noreply.github.com>
2022-11-29 14:14:38 +0100

    Add files via upload

---
+++ face_reco_webcam.py
@@ -0,0 +1,143 @@
+# Tutorials: https://www.youtube.com/watch?v=lC_y8wD7F3Y&t=173s&ab_channel=BrunoCenteno
+# Modifisert av Jacky Cao
+
+import numpy as np
+import face_recognition as fr
+import cv2
+import shutil, os
+import time
+
+
+video_capture = cv2.VideoCapture(0) # Starter video.
+
+
+
+path = "faces/"
+files1 = os.listdir(path)
+load_imgs = np.array([], ndmin=1)
+load_imgs1 = np.array(files1)
+combine = np.concatenate((load_imgs, load_imgs1))
+
+if os.path.exists(f"{path}.DS_Store"):
+    os.remove(f"{path}.DS_Store")
+
+
+kjent_ansikt_encode = [] # Rekkefølge er viktig. Ellers så viser navnet feil på kamera
+kjent_ansikt_navn = []
+
+for xfile in combine:
+    name, type = os.path.splitext(xfile)
+
+    alle_bilder = fr.load_image_file(f"{path}{xfile}")
+    alle_ansiktsencode = fr.face_encodings(alle_bilder)[0]
+
+    kjent_ansikt_encode.append(alle_ansiktsencode)
+    kjent_ansikt_navn.append(name)
+
+
+# Til å få ansiktet fra front
+face_cascade = cv2.CascadeClassifier("archive_ansiktkjennetegn/cascades/data/haarcascade_frontalface_alt2.xml")
+
+
+# Protocol
+active = True
+face_unknown = False
+
+while active: # Neste steg. Kjennetegne på video. Bruk av while True. Framme for framme
+    ret, frame = video_capture.read() # ret betyr boolean og returnerer true hvis frame er tilgjengelig. (Lurer på hvorfor den brukes ikke. Bør undersøke på internett) : frame is an image array vector captured based on the default frames per second defined explicitly or implicitly
+    small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25) # Endre størrelse på framme
+    #rgb_frame = small_frame[:, :, ::-1] # Fanger RGB på frammen. Printer ut RGB.
+    #print(frame)
+
+    ansikt_locations = fr.face_locations(small_frame) # Finner hvor er ansikt i framme. Kan være flere ansikter.
+    ansikt_encoding = fr.face_encodings(small_frame, ansikt_locations) # tar ramme fra kamera og sjekker fargen. Liksom hvor er ansiktet.
+
+    for (top, right, bottom, left), ansikt_encoding in zip(ansikt_locations, ansikt_encoding): # Location lager array for top right bottom og left. Encoding iterates top, right osv.
+
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+        
+
+        match = fr.compare_faces(kjent_ansikt_encode, ansikt_encoding) # Analysere ansikt. Sammenligne hvis koden kjenner musk eller meg eller hvilken ansikt (kjent_ansikt_encode)
+
+        navn = "Ukjent"
+
+        face_distance = fr.face_distance(kjent_ansikt_encode, ansikt_encoding) # Sammenligne mellom analysert ansikt - normal ansikt
+
+        best_match_index = np.argmin(face_distance) # Hvis koden kjenner ansikt, print ut spesifikk index. # np.argmin returnerer smallest mulig verdi
+
+
+        if match[best_match_index]: # Kjekker for den beste eller mest lignene ansikt
+            navn = kjent_ansikt_navn[best_match_index] # Hvis true, viser det "Elon Musk" eller hva du endrer på linje 11.
+            # print(best_match_index)
+        else:
+            for (x,y,w,h) in faces:
+                print("Ansikt ikke gjenkjent. Lager ny identitet...")
+                roi_color = frame[y:y+h, x:x+w]
+                creating = input("Skriv inn navn: ")
+
+                filess = os.listdir(path)
+
+                cv2.imwrite(f"{creating}.png", roi_color)
+
+                face_unknown = True
+                active = False
+
+
+
+        # if navn == "Jacky":
+        #     print("STOOPID CRIME FOUND!")
+        #     exit()
+            
+
+
+        # Lage rektangel
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 100, 0), 2) # frame for bildet, mest betyr størrelse på rektangel, farge og tykkhet av rektangel.
+
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 100, 0), cv2.FILLED) # Lag rektangel under.
+        font = cv2.FONT_HERSHEY_SIMPLEX # Velg hvilken font det skal se ut.
+        cv2.putText(frame, navn, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 2) # Plassering av navn, farger, tykkhet og størrelsen.
+
+        # print("Top:", top, "Right:", right, "Bottom:", bottom, "Left:", left)
+
+    cv2.imshow("Big Boi", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("d"): # waitKey er sikkert frame per second. Settes på 0, fryser video, 1000 er 1 sek per framme. 1 er den beste jeg kan kjøre FPS kjappere på. For å øke FPS, vet jeg bare å gjøre videoen mindre.
+        break
+
+
+video_capture.release() # Bli ferdig med video fra webcam
+cv2.destroyAllWindows() 
+
+time.sleep(2)    
+
+if face_unknown == True:
+    path = "faces/.."
+    files = os.listdir(path)
+
+# /Users/kungbao/Documents/VSCode/Python/Ansiktsgjenkjenning/face_reco_webcam.py
+
+
+    for file in files:
+        name, type = os.path.splitext(file)
+        type = type[1:] 
+        # print(type == "png", "=", type)
+
+        
+        if os.path.exists(path + "/" + "faces"): # Hvis faces mappe finnes, put png inn 
+
+            if type != "png":
+                continue
+            else:
+                shutil.move(path + "/" + name + "." + type, path + "/" + "faces")
+                    
+
+            
+    else:
+        print("Ferdig! Flyttet til 'faces' mappe.")
